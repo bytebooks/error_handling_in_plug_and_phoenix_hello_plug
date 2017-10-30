e61267b1e8a0d24141f90157a3fc1b92a7e6ba3d
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-10-30 13:52:10 +0530

    addition machine

--- lib/hello_plug/web.ex
+++ lib/hello_plug/web.ex
@@ -1,5 +1,34 @@
 defmodule HelloPlug.Web do
+  import Plug.Conn
   def init(opts), do: opts
 
-  def call(conn, _opts), do: Plug.Conn.send_resp(conn, :ok, "Hello!")
+  def call(conn, _opts) do
+    conn = fetch_query_params(conn)
+
+    cond do
+      conn.method == "GET" and conn.params == %{} ->
+        conn
+        |> put_resp_content_type("text/html")
+        |> send_resp(:ok, """
+                     <!doctype html>
+                     <form>
+                     <input placeholder="First operand" name="op1" />
+                     <input placeholder="Second operand" name="op2" />
+                     <button type="submit">Add</button>
+                     </form>
+                     """)
+      conn.method == "GET" ->
+        {op1, ""} = conn.params["op1"] |> Integer.parse
+        {op2, ""} = conn.params["op2"] |> Integer.parse
+
+        conn
+        |> put_resp_content_type("text/html")
+        |> send_resp(:ok, """
+                     <!doctype html>
+                     <a href="/">Go back</a>
+                     <p>The sum of #{op1} and #{op2} is #{op1+op2}</p>
+                     """)
+    end
+  end
+
 end
