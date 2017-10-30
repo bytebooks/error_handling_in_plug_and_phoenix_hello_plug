4feb1f23ccf152fdeb6aee62ddb7dd73ebaf6bea
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-10-30 12:58:59 +0530

    init

---
+++ lib/hello_plug/application.ex
@@ -0,0 +1,20 @@
+defmodule HelloPlug.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: HelloPlug.Worker.start_link(arg)
+      # {HelloPlug.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: HelloPlug.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
