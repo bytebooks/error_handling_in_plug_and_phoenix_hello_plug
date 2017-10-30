2487070bc04b451eeb5759f0f1f17c46a00094c9
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-10-30 14:07:04 +0530

    show nice error messages

--- lib/hello_plug/web.ex
+++ lib/hello_plug/web.ex
@@ -3,31 +3,41 @@ defmodule HelloPlug.Web do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    conn = fetch_query_params(conn)
+    try do
+      conn = fetch_query_params(conn)
 
-    cond do
-      conn.method == "GET" and conn.params == %{} ->
-        conn
-        |> put_resp_content_type("text/html")
-        |> send_resp(:ok, """
-                     <!doctype html>
-                     <form>
-                     <input placeholder="First operand" name="op1" />
-                     <input placeholder="Second operand" name="op2" />
-                     <button type="submit">Add</button>
-                     </form>
-                     """)
-      conn.method == "GET" ->
-        {op1, ""} = conn.params["op1"] |> Integer.parse
-        {op2, ""} = conn.params["op2"] |> Integer.parse
+      cond do
+        conn.method == "GET" and conn.params == %{} ->
+          conn
+          |> put_resp_content_type("text/html")
+          |> send_resp(:ok, """
+                       <!doctype html>
+                       <form>
+                       <input placeholder="First operand" name="op1" />
+                       <input placeholder="Second operand" name="op2" />
+                       <button type="submit">Add</button>
+                       </form>
+                       """)
+        conn.method == "GET" ->
+          {op1, ""} = conn.params["op1"] |> Integer.parse
+          {op2, ""} = conn.params["op2"] |> Integer.parse
 
-        conn
-        |> put_resp_content_type("text/html")
-        |> send_resp(:ok, """
-                     <!doctype html>
-                     <a href="/">Go back</a>
-                     <p>The sum of #{op1} and #{op2} is #{op1+op2}</p>
-                     """)
+          conn
+          |> put_resp_content_type("text/html")
+          |> send_resp(:ok, """
+                       <!doctype html>
+                       <a href="/">Go back</a>
+                       <p>The sum of #{op1} and #{op2} is #{op1+op2}</p>
+                       """)
+      end
+      rescue
+        err ->
+          require Logger
+          Logger.error("INPUT_ERROR #{inspect err}")
+          send_resp(conn, :bad_request, """
+                    <!doctype html>
+                    <h1 style='color:red;'>Gimme integers!</h1>
+                    """)
     end
   end
 
